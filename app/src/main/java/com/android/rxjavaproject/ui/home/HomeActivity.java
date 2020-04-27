dada7c208914fe88eb06f452e6925901654059d8
Nandom <paul4nank@gmail.com>
2020-04-27 12:59:34 +0100

    Buffer Transformation Operators with button clicks

--- app/src/main/java/com/android/rxjavaproject/ui/home/HomeActivity.java
+++ app/src/main/java/com/android/rxjavaproject/ui/home/HomeActivity.java
@@ -9,13 +9,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.android.rxjavaproject.R;
 import com.android.rxjavaproject.data.DataSource;
 import com.android.rxjavaproject.model.Task;
+import com.jakewharton.rxbinding3.view.RxView;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import kotlin.Unit;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -24,47 +30,49 @@ public class HomeActivity extends AppCompatActivity {
 
     private TextView text;
 
+    // global disposables object
+    CompositeDisposable disposables = new CompositeDisposable();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Observable.fromIterable(DataSource.createTaskList())
-                .map(new Function<Task, String>() {
+        // detect clicks to a button
+        RxView.clicks(findViewById(R.id.button))
+                .map(new Function<Unit, Integer>() { // convert the detected clicks to an integer
                     @Override
-                    public String apply(Task task) throws Exception {
-
-                        return task.getDescription();
+                    public Integer apply(Unit unit) throws Exception {
+                        return 1;
                     }
                 })
-                .subscribeOn(Schedulers.io())
+                .buffer(4, TimeUnit.SECONDS) // capture all the clicks during a 4 second interval
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<String>() {
+                .subscribe(new Observer<List<Integer>>() {
                     @Override
                     public void onSubscribe(Disposable d) {
-
+                        disposables.add(d); // add to disposables to you can clear in onDestroy
                     }
-
                     @Override
-                    public void onNext(String task) {
-                        Log.d(TAG, "onNext: " + task);
+                    public void onNext(List<Integer> integers) {
+                        Log.d(TAG, "onNext: You clicked " + integers.size() + " times in 4 seconds!");
                     }
-
                     @Override
                     public void onError(Throwable e) {
 
                     }
-
                     @Override
                     public void onComplete() {
 
                     }
                 });
-
     }
 
-    Function<Task, String> extractDescriptionFunction = task -> {
-        Log.d(TAG, "apply: doing work on thread: " + Thread.currentThread().getName());
-        return task.getDescription();
-    };
+    // make sure to clear disposables when the activity is destroyed
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposables.clear();
+    }
 }
