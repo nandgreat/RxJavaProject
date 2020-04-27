05177aef312e690396493dc998d206c151e70b27
Nandom <paul4nank@gmail.com>
2020-04-27 11:57:10 +0100

    Filter and Distinct Operators

--- app/src/main/java/com/android/rxjavaproject/ui/home/HomeActivity.java
+++ app/src/main/java/com/android/rxjavaproject/ui/home/HomeActivity.java
@@ -8,13 +8,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.android.rxjavaproject.R;
+import com.android.rxjavaproject.data.DataSource;
+import com.android.rxjavaproject.model.Task;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
+import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.ResponseBody;
 
@@ -25,47 +30,50 @@ public class HomeActivity extends AppCompatActivity {
 
     private TextView text;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        HomeViewModel viewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
-        try {
-            viewModel.makeFutureQuery().get()
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(new Observer<ResponseBody>() {
-                        @Override
-                        public void onSubscribe(Disposable d) {
-                            Log.d(TAG, "onSubscribe: called.");
-                        }
 
-                        @Override
-                        public void onNext(ResponseBody responseBody) {
-                            Log.d(TAG, "onNext: got the response from server!");
-                            try {
-                                Log.d(TAG, "onNext: " + responseBody.string());
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
+        //***** Filter Operator
+//        Observable<Task> taskObservable = Observable
+//                .fromIterable(DataSource.createTaskList())
+//                .filter(Task::isComplete)
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread());
 
-                        @Override
-                        public void onError(Throwable e) {
-                            Log.e(TAG, "onError: ", e);
-                        }
 
-                        @Override
-                        public void onComplete() {
-                            Log.d(TAG, "onComplete: called.");
-                        }
-                    });
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        //********* Distint Operator
+        Observable<Task> taskObservable = Observable
+                .fromIterable(DataSource.createTaskList())
+                .distinct(new Function<Task, String>() {
+                    @Override
+                    public String apply(Task task) throws Exception {
+                        return task.getDescription();
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+            @Override
+            public void onNext(Task task) {
+                Log.d(TAG, "onNext: This task matches the description: " + task.getDescription());
+            }
+            @Override
+            public void onError(Throwable e) {
+
+            }
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
     }
 }
