2436f602819e83065725e6db43d49e4c6da2fdbc
Nandom <paul4nank@gmail.com>
2020-04-27 14:56:55 +0100

    ThrottleFirst Transformation Operators with searchviews

--- app/src/main/java/com/android/rxjavaproject/ui/home/HomeActivity.java
+++ app/src/main/java/com/android/rxjavaproject/ui/home/HomeActivity.java
@@ -2,7 +2,9 @@ package com.android.rxjavaproject.ui.home;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
@@ -36,8 +38,7 @@ public class HomeActivity extends AppCompatActivity {
     // global disposables object
     CompositeDisposable disposables = new CompositeDisposable();
 
-    //ui
-    private SearchView searchView;
+    Button button;
 
     // vars
     private long timeSinceLastRequest; // for log printouts only. Not part of logic.
@@ -51,60 +52,38 @@ public class HomeActivity extends AppCompatActivity {
 
         timeSinceLastRequest = System.currentTimeMillis();
 
-        searchView = findViewById(R.id.search_view);
+
+        button = findViewById(R.id.button);
+
+        timeSinceLastRequest = System.currentTimeMillis();
+
+        // Set a click listener to the button with RxBinding Library
+        RxView.clicks(button)
+                .throttleFirst(4000, TimeUnit.MILLISECONDS) // Throttle the clicks so 500 ms must pass before registering a new click
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Unit>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposables.add(d);
+                    }
+                    @Override
+                    public void onNext(Unit unit) {
+                        Log.d(TAG, "onNext: time since last clicked: " + (System.currentTimeMillis() - timeSinceLastRequest));
+                        someMethod(); // Execute some method when a click is registered
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                    }
+                    @Override
+                    public void onComplete() {
+                    }
+                });    }
 
 
-        // create the Observable
-        Observable<String> observableQueryText = Observable
-                .create((ObservableOnSubscribe<String>) emitter -> {
-
-                    // Listen for text input into the SearchView
-                    searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                        @Override
-                        public boolean onQueryTextSubmit(String query) {
-                            return false;
-                        }
-
-                        @Override
-                        public boolean onQueryTextChange(final String newText) {
-                            if(!emitter.isDisposed()){
-                                emitter.onNext(newText); // Pass the query to the emitter
-                            }
-                            return false;
-                        }
-                    });
-                })
-                .debounce(1000, TimeUnit.MILLISECONDS) // Apply Debounce() operator to limit requests
-                .subscribeOn(Schedulers.io());
-
-        // Subscribe an Observer
-        observableQueryText.subscribe(new Observer<String>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                disposables.add(d);
-            }
-            @Override
-            public void onNext(String s) {
-                Log.d(TAG, "onNext: time  since last request: " + (System.currentTimeMillis() - timeSinceLastRequest));
-                Log.d(TAG, "onNext: search query: " + s);
-                timeSinceLastRequest = System.currentTimeMillis();
-
-                // method for sending a request to the server
-                sendRequestToServer(s);
-            }
-            @Override
-            public void onError(Throwable e) {
-            }
-            @Override
-            public void onComplete() {
-            }
-        });
-    }
-
-
-    // Fake method for sending a request to the server
-    private void sendRequestToServer(String query){
-        // do nothing
+    private void someMethod(){
+        timeSinceLastRequest = System.currentTimeMillis();
+        Toast.makeText(this, "You clicked a button", Toast.LENGTH_SHORT).show();
+        // do something
     }
 
     // make sure to clear disposables when the activity is destroyed
